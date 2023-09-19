b1dec71489c3ce9c418b9b2201e4d09a14f78cdc
thebjko <thebjko@gmail.com>
2023-09-20 07:42:01 +0900

    ingredients

---
+++ chapter04_factory/ingredients.py
@@ -0,0 +1,66 @@
+class Dough:
+    pass
+
+class Sauce:
+    pass
+
+class Clam:
+    pass
+
+class Cheese:
+    pass
+
+class Pepperoni:
+    pass
+
+class Veggies:
+    pass
+
+
+class ThinCrustDough(Dough):
+    pass
+
+class MarinaraSauce(Sauce):
+    pass
+
+class ReggianoCheese(Cheese):
+    pass
+
+class Garlic(Veggies):
+    pass
+
+class Onion(Veggies):
+    pass
+
+class Mushroom(Veggies):
+    pass
+
+class RedPepper(Veggies):
+    pass
+
+class SlicedPepperoni(Pepperoni):
+    pass
+
+class FreshClam(Clam):
+    pass
+
+class ThickCrustDough(Dough):
+    pass
+
+class MozzarellaCheese(Cheese):
+    pass
+
+class PlumTomatoSauce(Sauce):
+    pass
+
+class EggPlant(Veggies):
+    pass
+
+class BlackOlives(Veggies):
+    pass
+
+class Spinach(Veggies):
+    pass
+
+class FrozenClam(Clam):
+    pass
