e2da503d7daf5affb2b4c07c64b9db7a2fe4f2ed
thebjko <thebjko@gmail.com>
2023-09-20 07:41:57 +0900

    NY, Chicago Pizza Ingredient Factories

--- chapter04_factory/factory.py
+++ chapter04_factory/factory.py
@@ -1,4 +1,8 @@
+from abc import ABC, abstractmethod
+from chapter04_factory.ingredients import Cheese, Clam, Dough, Pepperoni, Sauce, Veggies
+
 from pizzas import *
+from ingredients import *
 
 class SimplePizzaFactory:
     def __init__(self, pizza_type: str):
@@ -15,3 +19,72 @@ class SimplePizzaFactory:
             pizza = VeggiePizza()
 
         return self.pizza
+
+
+class PizzaIngredientFactory(ABC):
+    @abstractmethod
+    def create_dough(self) -> Dough:
+        pass
+
+    @abstractmethod
+    def create_sauce(self) -> Sauce:
+        pass
+
+    @abstractmethod
+    def create_cheese(self) -> Cheese:
+        pass
+
+    @abstractmethod
+    def create_veggies(self) -> list[Veggies]:
+        pass
+
+    @abstractmethod
+    def create_pepperoni(self) -> Pepperoni:
+        pass
+
+    @abstractmethod
+    def create_clam(self) -> Clam:
+        pass
+
+
+class NYPizzaIngredientFactory(PizzaIngredientFactory):
+    def create_cheese(self) -> Cheese:
+        return ReggianoCheese()
+    
+    def create_sauce(self) -> Sauce:
+        return MarinaraSauce()
+    
+    def create_veggies(self) -> list[Veggies]:
+        veggies = [Garlic(), Onion(), Mushroom(), RedPepper()]
+        return veggies
+    
+    def create_pepperoni(self) -> Pepperoni:
+        return SlicedPepperoni()
+    
+    def create_clam(self) -> Clam:
+        return FreshClam()
+    
+    def create_dough(self) -> Dough:
+        return ThinCrustDough
+    
+
+class ChicagoPizzaIngredientFactory(PizzaIngredientFactory):
+    def create_cheese(self) -> Cheese:
+        return MozzarellaCheese()
+    
+    def create_sauce(self) -> Sauce:
+        return PlumTomatoSauce()
+    
+    def create_veggies(self) -> list[Veggies]:
+        veggies = [EggPlant(), Spinach(), BlackOlives()]
+        return veggies
+    
+    def create_pepperoni(self) -> Pepperoni:
+        return SlicedPepperoni()
+    
+    def create_clam(self) -> Clam:
+        return FrozenClam()
+    
+    def create_dough(self) -> Dough:
+        return ThickCrustDough
+    
