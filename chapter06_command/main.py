281847e8deaa81f826276a7613d20c89e4d78636
thebjko <thebjko@gmail.com>
2023-09-23 17:24:18 +0900

    feat: simple command pattern

--- chapter06_command/main.py
+++ chapter06_command/main.py
@@ -8,6 +8,11 @@ class Command(metaclass=ABCMeta):
         pass
 
 
+class Light:
+    def on(self):
+        print('light is on')
+
+
 class LightOnCommand(Command):
     def __init__(self, light):
         self.light = light
@@ -16,13 +21,49 @@ class LightOnCommand(Command):
         self.light.on()
 
 
+class GarageDoor:
+    def up(self):
+        print('door is upping')
+    
+    def down(self):
+        print('door is downing')
+
+    def stop(self):
+        print('door has stopped')
+    
+    def light_on(self):
+        print('light on')
+    
+    def light_off(self):
+        print('light off')
+
+
+class GarageDoorOpenCommand(Command):
+    def __init__(self, door):
+        self.door = door
+
+    def execute(self) -> None:
+        self.door.up()
+
+
 class SimpleRemoteControl:
-    def __init__(self, command: Command):
-        self.slot = command
+    def __init__(self):
+        self.slot = None
 
     def button_was_pressed(self):
         self.slot.execute()
 
+
 if __name__ == '__main__':
+    light = Light()
+    light_on = LightOnCommand(light)
+
+    door = GarageDoor()
+    garage_open = GarageDoorOpenCommand(door)
+
     rc = SimpleRemoteControl()
+    rc.slot = light_on
+    rc.button_was_pressed()
+
+    rc.slot = garage_open   # 종업원?
     rc.button_was_pressed()
