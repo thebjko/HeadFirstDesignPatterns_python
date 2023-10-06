78865007d4e6d92bdad903da2238e8b6c91ed35f
고봉진 <thebjko@gmail.com>
2023-10-07 08:51:27 +0900

    refactor: delete comments

--- chapter08_template/main.py
+++ chapter08_template/main.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 
 
 class CaffeineBeverage(metaclass=ABCMeta):
+
     def prepare_recipe(self):
         self.boil_water()
         self.brew()
@@ -25,42 +26,22 @@ class CaffeineBeverage(metaclass=ABCMeta):
 
 
 class Coffee(CaffeineBeverage):
-    # def prepare_recipe(self):
-    #     super().prepare_recipe()
 
     def brew(self):
         print('brewing coffee')
-        # self.brew_coffee_grinds()
 
     def add_condiments(self):
-        print('adding sugar and milk')
-        # self.add_sugar_and_milk()
-
-    def brew_coffee_grinds(self):
-        print('brewing coffee')
-
-    def add_sugar_and_milk(self):
         print('adding sugar and milk')
 
 
 class Tea(CaffeineBeverage):
-    # def prepare_recipe(self):
-    #     super().prepare_recipe()
 
     def brew(self):
         print('brewing tea')
-        # self.steep_tea_bag()
 
     def add_condiments(self):
         print('adding lemon')
-        # self.add_lemon()
-    
-    def steep_tea_bag(self):
-        print('brewing tea')
 
-    def add_lemon(self):
-        print('adding lemon')
-    
 
 if __name__ == '__main__':
     c = Coffee()
