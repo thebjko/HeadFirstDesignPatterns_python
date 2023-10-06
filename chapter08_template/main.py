3a1c3169bab60f82080ae4e0e4091fa596a750d7
고봉진 <thebjko@gmail.com>
2023-10-07 08:48:00 +0900

    apply template method pattern

--- chapter08_template/main.py
+++ chapter08_template/main.py
@@ -2,45 +2,69 @@ from abc import ABCMeta, abstractmethod
 
 
 class CaffeineBeverage(metaclass=ABCMeta):
-    @abstractmethod
     def prepare_recipe(self):
         self.boil_water()
         self.brew()
         self.pour_in_cup()
         self.add_condiments()
 
+    @abstractmethod
+    def brew(self):
+        pass
+
+    @abstractmethod
+    def add_condiments(self):
+        pass
+
     def boil_water(self):
         print('boiling water')
 
     def pour_in_cup(self):
         print('pouring beverage')
-        
+
 
 
 class Coffee(CaffeineBeverage):
-    def prepare_recipe(self):
-        super().prepare_recipe()
+    # def prepare_recipe(self):
+    #     super().prepare_recipe()
+
+    def brew(self):
+        print('brewing coffee')
+        # self.brew_coffee_grinds()
+
+    def add_condiments(self):
+        print('adding sugar and milk')
+        # self.add_sugar_and_milk()
 
     def brew_coffee_grinds(self):
-        pass
+        print('brewing coffee')
 
     def add_sugar_and_milk(self):
-        pass
+        print('adding sugar and milk')
 
 
 class Tea(CaffeineBeverage):
-    def prepare_recipe(self):
-        super().prepare_recipe()
+    # def prepare_recipe(self):
+    #     super().prepare_recipe()
+
+    def brew(self):
+        print('brewing tea')
+        # self.steep_tea_bag()
+
+    def add_condiments(self):
+        print('adding lemon')
+        # self.add_lemon()
     
     def steep_tea_bag(self):
-        pass
+        print('brewing tea')
 
     def add_lemon(self):
-        pass
-
+        print('adding lemon')
     
+
 if __name__ == '__main__':
     c = Coffee()
-    c.boil_water()
     c.prepare_recipe()
 
+    t = Tea()
+    t.prepare_recipe()
