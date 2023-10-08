eb130a92e7997a37f025e8e47c86097d7f2a0ac9
고봉진 <thebjko@gmail.com>
2023-10-09 08:23:45 +0900

    comparable ducks

---
+++ chapter08_template/comparable_ducks.py
@@ -0,0 +1,28 @@
+class ComparableDuck:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def __repr__(self):
+        return f'{self.name} weight: {self.weight}'
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+    def __le__(self, other):
+        return self.weight <= other.weight
+
+
+if __name__ == '__main__':
+    ducks = [
+        ComparableDuck('Daffy', 8),
+        ComparableDuck('Dewey', 2),
+        ComparableDuck('Howard', 7),
+        ComparableDuck('Louie', 2),
+        ComparableDuck('Donald', 10),
+        ComparableDuck('Huey', 2),
+    ]
+    print(ducks)
+
+    ducks.sort()
+    print(ducks)
