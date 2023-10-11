ac1e14759da6f6064a5a185e10c22103fb6e9fba
고봉진 <thebjko@gmail.com>
2023-10-12 08:30:40 +0900

    feat: add waitress

---
+++ chapter09_iterator_composite/waitress.py
@@ -0,0 +1,21 @@
+from iterators import PythonicIterator
+
+
+class Waitress:
+    def __init__(self, pancakehouse_menu, diner_menu):
+        self.pancakehouse_menu = pancakehouse_menu
+        self.diner_menu = diner_menu
+
+    def print_menu(self):
+        pancake_iterator = self.pancakehouse_menu.create_iterator()
+        diner_iterator = self.diner_menu.create_iterator()
+
+        print('메뉴\n---\n아침 메뉴')
+        self._print_menu(pancake_iterator)
+        print('\n점심 메뉴')
+        self._print_menu(diner_iterator)
+
+    def _print_menu(self, iterator):
+        while iterator.has_next():
+            menu_item = next(iterator)
+            print(f'{menu_item.name}, {menu_item.price} -- {menu_item.description}')
