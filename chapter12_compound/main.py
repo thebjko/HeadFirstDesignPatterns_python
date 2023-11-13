5e389c58083b9d8f9f0da2df1524329df70ef705
thebjko <thebjko@gmail.com>
2023-11-14 08:43:11 +0900

    feat: GooseAdapter

--- chapter12_compound/main.py
+++ chapter12_compound/main.py
@@ -28,11 +28,25 @@ class RubberDuck(Quackable):
         print("삑삑")
 
 
+class Goose:
+    def honk(self):
+        print("끽끽")
+
+
+class GooseAdapter(Quackable):
+    def __init__(self, goose):
+        self.goose = goose
+    
+    def quack(self):
+        self.goose.honk()
+
+
 if __name__ == "__main__":
     mallard_duck = MallardDuck()
     redhead_duck = RedheadDuck()
     duck_call = DuckCall()
     rubber_duck = RubberDuck()
+    goose_duck = GooseAdapter(Goose())
 
     print("오리 시뮬레이션 게임")
 
@@ -43,3 +57,4 @@ if __name__ == "__main__":
     simulate(redhead_duck)
     simulate(duck_call)
     simulate(rubber_duck)
+    simulate(goose_duck)
