10a697a03d24f3114ab9240fbaab0de98084b26d
thebjko <thebjko@gmail.com>
2023-11-14 08:59:38 +0900

    feat: CountingDuckFactory

--- chapter12_compound/main.py
+++ chapter12_compound/main.py
@@ -55,13 +55,59 @@ class QuackCounter(Quackable):
     def counter(cls):
         cls.number_of_quacks += 1
         
+
+class AbstractDuckFactory(ABC):
+    @abstractmethod
+    def create_mallard_duck(self):
+        pass
+
+    @abstractmethod
+    def create_redhead_duck(self):
+        pass
+
+    @abstractmethod
+    def create_duck_call(self):
+        pass
+
+    @abstractmethod
+    def create_rubber_duck(self):
+        pass
+
+
+class DuckFactory(AbstractDuckFactory):
+    def create_mallard_duck(self):
+        return MallardDuck()
     
+    def create_redhead_duck(self):
+        return RedheadDuck()
+    
+    def create_duck_call(self):
+        return DuckCall()
+    
+    def create_rubber_duck(self):
+        return RubberDuck()
+
+
+class CountingDuckFactory(AbstractDuckFactory):
+    def create_mallard_duck(self):
+        return QuackCounter(MallardDuck())
+    
+    def create_redhead_duck(self):
+        return QuackCounter(RedheadDuck())
+    
+    def create_duck_call(self):
+        return QuackCounter(DuckCall())
+    
+    def create_rubber_duck(self):
+        return QuackCounter(RubberDuck())
+
 
 if __name__ == "__main__":
-    mallard_duck = QuackCounter(MallardDuck())
-    redhead_duck = QuackCounter(RedheadDuck())
-    duck_call = QuackCounter(DuckCall())
-    rubber_duck = QuackCounter(RubberDuck())
+    duck_factory = CountingDuckFactory()
+    mallard_duck = duck_factory.create_mallard_duck()
+    redhead_duck = duck_factory.create_redhead_duck()
+    duck_call = duck_factory.create_duck_call()
+    rubber_duck = duck_factory.create_rubber_duck()
     goose_duck = GooseAdapter(Goose())
 
     print("오리 시뮬레이션 게임")
