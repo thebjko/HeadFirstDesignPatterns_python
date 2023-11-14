0ba3412f99127644383754c9d4c39f47463cc3c6
thebjko <thebjko@gmail.com>
2023-11-14 09:10:02 +0900

    feat: composite pattern - Flock

--- chapter12_compound/main.py
+++ chapter12_compound/main.py
@@ -102,6 +102,18 @@ class CountingDuckFactory(AbstractDuckFactory):
         return QuackCounter(RubberDuck())
 
 
+class Flock(Quackable):
+    def __init__(self):
+        self.quackers = []
+
+    def add(self, quacker):
+        self.quackers.append(quacker)
+
+    def quack(self):
+        for quacker in self.quackers:
+            quacker.quack()
+
+
 if __name__ == "__main__":
     duck_factory = CountingDuckFactory()
     mallard_duck = duck_factory.create_mallard_duck()
@@ -110,15 +122,35 @@ if __name__ == "__main__":
     rubber_duck = duck_factory.create_rubber_duck()
     goose_duck = GooseAdapter(Goose())
 
-    print("오리 시뮬레이션 게임")
+    print("오리 시뮬레이션 게임: 무리 (+컴포지트)")
+
+    flock_of_ducks = Flock()
+
+    flock_of_ducks.add(redhead_duck)
+    flock_of_ducks.add(duck_call)
+    flock_of_ducks.add(rubber_duck)
+    flock_of_ducks.add(goose_duck)
+
+    flock_of_mallards = Flock()
+    mallard_one = duck_factory.create_mallard_duck()
+    mallard_two = duck_factory.create_mallard_duck()
+    mallard_three = duck_factory.create_mallard_duck()
+    mallard_four = duck_factory.create_mallard_duck()
+
+    flock_of_mallards.add(mallard_one)
+    flock_of_mallards.add(mallard_two)
+    flock_of_mallards.add(mallard_three)
+    flock_of_mallards.add(mallard_four)
+
+    flock_of_ducks.add(flock_of_mallards)
 
     def simulate(duck):
         duck.quack()
 
-    simulate(mallard_duck)
-    simulate(redhead_duck)
-    simulate(duck_call)
-    simulate(rubber_duck)
-    simulate(goose_duck)
+    print("오리 시뮬레이션 게임: 전체 무리")
+    simulate(flock_of_ducks)
+
+    print("오리 시뮬레이션 게임: 물오리 무리")
+    simulate(flock_of_mallards)
 
     print("오리가 소리 낸 횟수 : {}번".format(QuackCounter.number_of_quacks))
